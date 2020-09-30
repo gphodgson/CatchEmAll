1a0f02b4c886f90e69591b12ae63438cf3c6ae57
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-09-30 00:20:40 -0500

    Added next & previous arrows to Pokemon show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,5 +5,7 @@ class PokemonsController < ApplicationController
 
   def show
     @pokemon = Pokemon.find(params[:id])
+    @prev_pokemon = Pokemon.find_by(id: @pokemon.id - 1)
+    @next_pokemon = Pokemon.find_by(id: @pokemon.id + 1)
   end
 end
