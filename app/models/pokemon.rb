76074983a8c52c06d85970fd0b12ff58d9b50376
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-09-29 19:59:18 -0500

    Added docs for Pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,6 +3,19 @@ class Pokemon < ApplicationRecord
   # Pokemon
   #
   # Represents Pokemon table in database.
+  #
+  # Feilds
+  # --------------------------
+  # Name:           | String  | Required
+  # Pokedex_Number  | Integer | Required, Numeric, Whole Number
+  # Thumb:          | Text    | Required
+  #
+  # Assocations
+  # --------------------
+  #
+  #======================
+
+  has_one :stat
 
   validates :name, :pokedex_number, :thumb, presence: true
   validates :pokedex_number, numericality: { only_integer: true }
