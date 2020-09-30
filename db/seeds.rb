8917b6b0a2a38c3002e88eb42bc134c221d729ea
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-09-30 15:26:07 -0500

    Added Pokemons.color to seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require "net/http"
 require "json"
 require "pp"
+require "rmagick"
 
 Pokemon.delete_all
 Stat.delete_all
@@ -15,11 +16,19 @@ response["results"].each do |pokemon_ref|
 
   pokemon_res = JSON.parse(Net::HTTP.get(uri))
 
+  image = Magick::ImageList.new
+  urlimage = URI.open("https://pokeres.bastionbot.org/images/pokemon/#{pokemon_res['id']}.png") # Image Remote URL
+  image.from_blob(urlimage.read)
+
+  pix = image.scale(1, 1)
+  average_color = pix.pixel_color(0, 0).to_color[0..6]
+
   pokemon = Pokemon.new(
     name:           pokemon_res["name"],
     pokedex_number: pokemon_res["id"],
     img:            "https://pokeres.bastionbot.org/images/pokemon/#{pokemon_res['id']}.png",
-    thumb:          pokemon_res["sprites"]["versions"]["generation-i"]["yellow"]["front_default"]
+    thumb:          pokemon_res["sprites"]["versions"]["generation-i"]["yellow"]["front_default"],
+    color:          average_color
   )
 
   if pokemon&.valid?
