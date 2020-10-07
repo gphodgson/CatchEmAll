2d0d1ea9b1c3f34507296f94d32a096bf77264e7
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-10-07 18:17:12 -0500

    Added search route

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -8,4 +8,8 @@ class PokemonsController < ApplicationController
     @prev_pokemon = Pokemon.find_by(id: @pokemon.id - 1)
     @next_pokemon = Pokemon.find_by(id: @pokemon.id + 1)
   end
+
+  def search
+    @pokemons = Pokemon.where("name LIKE :query", query: "%#{params[:search_term]}%")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :stats, only: [:show]
+  get "/pokemons/search/:search_term", to: "pokemons#search"
   resources :pokemons, only: %i[index show]
 end
