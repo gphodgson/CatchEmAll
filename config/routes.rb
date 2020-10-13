69f3258d33bb0e2d0598ab9c0b1d0d82c967a7b8
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-10-12 21:13:33 -0500

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :types, only: [:index, :show]
-  resources :locations, only: [:index, :show]
+  resources :about, only: [:index]
+  resources :types, only: %i[index show]
+  resources :locations, only: %i[index show]
   resources :stats, only: [:show]
   get "/pokemons/search", to: "pokemons#search"
   resources :pokemons, only: %i[index show]
