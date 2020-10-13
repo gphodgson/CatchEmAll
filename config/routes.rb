93265e8df46e67efe1c45da0436645f2c14903c4
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-10-12 21:33:15 -0500

    made index got to Pokemons index

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :stats, only: [:show]
   get "/pokemons/search", to: "pokemons#search"
   resources :pokemons, only: %i[index show]
+  get "/", to: "pokemons#index"
 end
