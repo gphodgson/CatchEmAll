5f5810972f5454c30fa225c261781a571b9bcc3e
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-11-05 19:09:45 -0600

    Added validation to Checklist

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,2 +1,19 @@
+#======================
+# Location
+#
+# Represents Encounters table in database.
+#
+# Feilds
+# --------------------------
+# Title           | String  | Not Required
+# Unique Url Id   | String  | Required, Unique
+#
+# Associations
+# --------------------
+#
+#======================
+
 class Checklist < ApplicationRecord
+  validates :unique_url_id, presence: true
+  validates :unique_url_id, uniqueness: true
 end
